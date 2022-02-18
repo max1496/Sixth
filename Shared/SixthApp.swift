2391377d2e3971b082c4f80a846b12f3daa89ef2
yongsoo <max1495@naver.com>
2022-02-18 12:04:31 +0900

    Initial Commit

---
+++ Shared/SixthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixthApp.swift
+//  Shared
+//
+//  Created by Yongsoo Chang on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SixthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
